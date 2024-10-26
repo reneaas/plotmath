fd1996808bc7b5b9cfa58be81c55a26d170e9fc9
reneaas <reneaas@uio.no>
2024-10-26 17:33:41 +0200

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="plotmath",  # Replace with your desired package name
-    version="0.1.7",
+    version="0.1.8",
     author="René Alexander Ask",
     author_email="rene.ask@icloud.com",
     description="Automatically generates textbook graphs for mathematical functions.",
