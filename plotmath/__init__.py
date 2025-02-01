2b3d007ff8bea3cf5a9b3fb07325ad887ac57510
reneaas <reneaas@uio.no>
2025-02-01 16:44:34 +0100

    update

--- plotmath/__init__.py
+++ plotmath/__init__.py
@@ -1,3 +1,3 @@
-from .plotmath import plot, show, savefig, histogram
+from .plotmath import plot, show, savefig, histogram, plot_polygon
 
 __all__ = ["plot", "savefig", "show", "histogram", "plot_polygon"]
