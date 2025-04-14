ee16718ef7c2b203173db9e4c4601e767c0d5acc
reneaas <reneaas@uio.no>
2025-04-15 00:37:49 +0200

    update

--- plotmath/plotmath.py
+++ plotmath/plotmath.py
@@ -21,7 +21,7 @@ if latex_available:
 else:
     plt.rc("text", usetex=False)
 
-colors = ["teal", "royalblue", "crimsomred", "darkorchid", "navy"]
+colors = ["teal", "royalblue", "crimson", "darkorchid", "navy"]
 # colors = np.random.permutation(colors)
 plt.rcParams["axes.prop_cycle"] = plt.cycler(color=colors)
 
